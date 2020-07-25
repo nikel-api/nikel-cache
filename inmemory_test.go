1e08f889218f434d5b639b69c2578ef70c014f0a
Daren Liang <daren@darenliang.com>
2020-07-25 11:58:14 -0400

    Clean up tests

--- inmemory_test.go
+++ inmemory_test.go
@@ -53,42 +53,6 @@ func TestIMRemove(t *testing.T) {
 	expect(t, len(im.hash), 0)
 }
 
-// TestIMUpdate tests Update
-func TestIMUpdate(t *testing.T) {
-	im := InMemory{
-		hash: map[string][]byte{
-			"key": {1, 2, 3},
-		},
-	}
-
-	err := im.Update("key", []byte{1})
-	expect(t, err, nil)
-	expect(t, len(im.hash["key"]), 1)
-
-	err = im.Update("__key__", []byte{1})
-	expect(t, err, errNotFound)
-}
-
-// TestIMKeys tests Keys
-func TestIMKeys(t *testing.T) {
-	im := InMemory{
-		hash: map[string][]byte{
-			"key": {1, 2, 3},
-		},
-	}
-
-	keys := im.Keys()
-	expect(t, len(keys), 1)
-	expect(t, keys[0], "key")
-
-	im.hash["__key__"] = []byte{}
-
-	keys = im.Keys()
-	expect(t, len(keys), 2)
-	expect(t, keys[0], "key")
-	expect(t, keys[1], "__key__")
-}
-
 func expect(t *testing.T, a interface{}, b interface{}) {
 	if a != b {
 		t.Errorf("Expected %v (type %v) - Got %v (type %v)", b, reflect.TypeOf(b), a, reflect.TypeOf(a))
